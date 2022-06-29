951a91c066c15658f28a1ab22da6f34ee560c0a6
Alexinder Virdee <alex.virdee08@gmail.com>
2022-06-29 15:12:56 -0400

    Type safety around serialize

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,12 +1,12 @@
-import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
-@Serialize(UserDto)
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
@@ -18,7 +18,6 @@ export class UsersController {
    
     @Get('/:id')
     async findUser(@Param('id') id: string) {
-        console.log('handler is running')
         const user = await this.usersService.findOne(parseInt(id));
 
         if (!user) {
