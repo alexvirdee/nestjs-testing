3d15d68f05cfe858d3f70b82a347565e25f87d10
Alexinder Virdee <alex.virdee08@gmail.com>
2022-06-30 11:40:34 -0400

    Added in signin functionality

--- mycv/src/users/auth.service.ts
+++ mycv/src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -28,11 +28,27 @@ export class AuthService {
         const result = salt + '.' + hash.toString('hex');
 
         // Create a new user and save it
+        const user = await this.usersService.create(email, result);
 
         // return the user
+        return user;
     }
 
-    signin() {
+    async signin(email: string, password: string) {
+        const [user] = await this.usersService.find(email);
 
+        if (!user) {
+            throw new NotFoundException('user not found')
+        }
+
+        const [salt, storedHash] = user.password.split('.');
+
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+        if (storedHash !== hash.toString('hex')) {
+            throw new BadRequestException('bad password')
+        }
+        
+        return user;
     }
 }
