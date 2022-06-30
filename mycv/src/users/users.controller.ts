6ad655a714eabfaaae3d1e1be30dca03e235672a
Alexinder Virdee <alex.virdee08@gmail.com>
2022-06-30 12:04:48 -0400

    Get the current user as well as sign out

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, Session } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -14,14 +14,34 @@ export class UsersController {
         private authService: AuthService
         ) {}
 
-    @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        return this.authService.signup(body.email, body.password)
+    @Get('/whoami')
+    whoAmI(@Session() session: any) {
+        const user = this.usersService.findOne(session.userId);
+
+        if (!user) {
+            throw new NotFoundException('user not found')
+        }
+
+        return user;
+    }
+
+   @Post('signout')
+   signOut(@Session() session: any) {
+    session.userId = null;
+   }
+
+   @Post('/signup')
+   async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+       const user = await this.authService.signup(body.email, body.password);
+       session.userId = user.id;
+       return user;
     }
 
     @Post('/signin')
-    signin(@Body() body: CreateUserDto) {
-        return this.authService.signin(body.email, body.password);
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
    
